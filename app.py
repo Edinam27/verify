832b759a8e0bc6f2495e4a14b171d09427dd7375
Edinam27 <159185604+Edinam27@users.noreply.github.com>
2025-04-26 15:43:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -311,4 +311,5 @@ def add_program():
 if __name__ == '__main__':
     # Create upload folder if it doesn't exist
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)

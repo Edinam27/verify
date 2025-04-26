a53ebbca22e6179ad90a7e41f6819e45d4cd9e8c
Edinam27 <159185604+Edinam27@users.noreply.github.com>
2025-04-26 12:27:51 +0000

    Update app.py

--- app.py
+++ app.py
@@ -213,5 +213,5 @@ def admin():
     return render_template('admin.html', attendance_records=attendance_records)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Render will give a PORT env var
-    app.run(host='0.0.0.0', port=port)
+    
+    app.run(host='0.0.0.0')
